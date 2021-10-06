f1a811445b1634c77229ea84bb66ec5ed5d425d3
reece <vbnree001@myuct.ac.za>
2021-10-06 20:35:30 +0100

    added simple formatting for later

--- prac4.py
+++ prac4.py
@@ -1,5 +1,9 @@
+
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
+
+#for waiting
+import time
 
 import busio
 import digitalio
@@ -7,6 +11,9 @@ import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
+
+runtime = 0
+temp = 0
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -20,7 +27,18 @@ mcp = MCP.MCP3008(spi, cs)
 chan = AnalogIn(mcp, MCP.P0)
 
 # create an analog input channel on pin 1
-chan_2 = AnalogIn(mcp, MCP.P1)
+chan2 = AnalogIn(mcp, MCP.P2)
 
-print("Raw ADC Value: ", chan.value)
-print("ADC Voltage: " + str(chan.voltage) + "V")
+#while True:
+	#print("Raw ADC Value: ", chan.value)
+	#print("ADC Voltage: " + str(chan.voltage) + "V")
+
+
+while True:
+	print("Runtime   Temp Reading   Temp      Light Reading")
+	print(str(runtime).ljust(9,' '), 	#runtime
+	str(chan.value).ljust(14,' '), 		#temp adc
+	(str(temp)+"C").ljust(9,' '), 		#temp C
+	chan2.value) 				#light resistor reading
+	time.sleep(1)
+	runtime+=1
