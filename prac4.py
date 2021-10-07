a61259ca41225f882fc9fcf8242a278d6ee67c9d
LXXHSI007 <lxxhsi007@myuct.ac.za>
2021-10-07 03:54:29 +0200

    prac4.py: add a thread class and did some button stuff

--- prac4.py
+++ prac4.py
@@ -3,13 +3,15 @@
 
 #placeholder import for now
 import time
-
+import threading
 import busio
 import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
+
+#SETUP
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -24,6 +26,10 @@ ADC = AnalogIn(mcp, MCP.P1)
 
 # create an analog input channel on pin 1
 LDR = AnalogIn(mcp, MCP.P2)
+#set up button
+button = digitalio.DigitalInOut(board.D26)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
 
 def ConvertTemp(data):
 
@@ -37,6 +43,31 @@ def ConvertTemp(data):
 
 #temp runtime
 runtime = 0
+
+class MyThread(threading.Thread):
+  def __init__(self):
+    self.runtime = 10
+    self.lock = threading.RLock()
+    super(MyThread, self).__init__()
+  
+  def set_time(self):
+    if self.runtime == 10:
+      self.runtime = 5
+    elif self.runtime == 5:
+      self.runtime = 1
+    else:
+      self.runtime = 10
+  
+  def run(self):
+    while True:
+      print(str(self.runtime).ljust(9,' '), 	#runtime
+      str(ADC.value).ljust(14,' '), 		#temp adc
+      (str(ConvertTemp(ADC.voltage))+"C").ljust(9,' '), 		    #temp C
+      LDR.value) 				                #light resistor reading
+      
+      #remove later
+      time.sleep(self.runtime)
+
 
 
 def PrintTable():
@@ -53,5 +84,15 @@ def PrintTable():
 	  time.sleep(1)
 	  runtime+=1
 
+
+
 if __name__ == "__main__":
-  PrintTable()
+  print("Runtime   Temp Reading   Temp      Light Reading")
+  interval = 10
+  blink_thread = MyThread()
+  blink_thread.start()
+  while True:
+    if button.value == False:
+      time.sleep(0.25)
+      blink_thread.set_time()
+      
